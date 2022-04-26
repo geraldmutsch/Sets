b652c4e4e294a2e6d8aff5940be14aec4819af90
geraldmutsch <102688282+geraldmutsch@users.noreply.github.com>
2022-04-26 19:32:51 +0000

    compare

---
+++ main.py
@@ -0,0 +1,10 @@
+#lets compare sets and lists
+
+
+fruits = {'banana', 'apple','pear'}
+fruits.add('banana')
+print(fruits)
+
+veggies = ['cucumber', 'tomatoe','salad']
+veggies.append('tomatoe')
+print(veggies)
