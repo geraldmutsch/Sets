6828b061661b8015aabff83b341769ee2cf41bb9
geraldmutsch <102688282+geraldmutsch@users.noreply.github.com>
2022-04-26 19:36:34 +0000

    real tomatoes

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ fruits = {'banana', 'apple','pear'}
 fruits.add('banana')
 print(fruits)
 
-veggies = ['cucumber', 'tomatoe','salad']
-veggies.append('tomatoe')
+veggies = ['cucumber', 'tomato','salad']
+veggies.append('tomato')
 print(veggies)
